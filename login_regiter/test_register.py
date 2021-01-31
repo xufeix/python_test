dfba89ad8315c96039035d4ffd61e4d129b68a5f
xufei <17600258606@163.com>
2021-01-31 22:28:34 +0800

    类的多继承

---
+++ login_regiter/test_register.py
@@ -0,0 +1,28 @@
+# /usr/bin/env python3.8
+# coding:utf-8
+
+"""
+@file: test_register.py
+@time: 2021/1/30 9:14 下午
+@author:XF
+"""
+
+import pymysql
+
+# 注册
+name = input("请输入姓名：")
+passwd = input("请输入秘密：")
+
+# 插入数据库
+
+register_sql = "insert into user_account(`name`,`passwd`) values('" + name + "','" + passwd + "')"
+
+# 打开数据库连接
+db = pymysql.connect(host='127.0.0.1', user= 'root', password='', database='user_account')
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+# 使用 execute()  方法执行 SQL 查询
+print(cursor.execute(register_sql))
+
+db.commit()
+
